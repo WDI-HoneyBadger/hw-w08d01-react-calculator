b5b5d349802b35da5acab8e9dacd0434d0fe9c72
hamad-SHE <eng_cis1@hotmail.com>
2019-01-07 19:20:16 +0300

    done

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,18 +3,97 @@ import Display from './Display';
 import NumberPad from './NumberPad';
 
 class Calculator extends Component {
-  // this component may need to have some state
-  // think about what you need to keep track of
-  // where would you need to pass information to?
+ 
+  constructor (){
+    super();
+    this.state={
+      showNumber : [],
+      process : '',
+      oneElemnt : [],
+      towElemnt : []
+    }
+  }
+  renderProcsess(symbol,number){
+    if(number === true || symbol ==='.'){
+      const newN = this.state.oneElemnt.concat([symbol])
+      this.setState({
+        showNumber : newN ,
+        oneElemnt : newN
+      })
+    }else if(symbol==='='){
+      this.RenderScore();
+  
+    }else if(symbol === 'C'){
+      this.setState({
+        showNumber :[],
+        process :[],
+        oneElemnt :[],
+        towElemnt : []
+  
+      })
+    }else if (symbol !== 'C' && number === false && symbol !=='.'){
+      this.setState({
+        process: symbol
+      })
+    }else if((number === true || symbol ==='.') && this.state.process !== ''){
+      const NewNum = this.state.towElemnt.concat([symbol])
+      this.setState({
+        showNumber:NewNum,
+        towElemnt : NewNum
+      })
+    }
+  }
+  
+
+    
+  
+  RenderScore(){
+    let oneElemnt = this.state.oneElemnt;
+    let towElemnt = this.state.towElemnt;
+    let process = this.state.process;
+  
+    oneElemnt = oneElemnt.join('');
+    towElemnt = towElemnt.join('');
+    oneElemnt= Number(oneElemnt);
+    towElemnt= Number(towElemnt);
+
+  
+
+  let result;
+
+  switch(process){
+    case '+': result = oneElemnt + towElemnt;
+    break;
+    case '-': result = oneElemnt - towElemnt;
+    break;
+    case '/': result= oneElemnt - towElemnt;
+    break;
+    case "x": result= oneElemnt * towElemnt;
+    break;
+    case "%": result = oneElemnt % towElemnt;
+    break; 
+    default : console.log('not found')
+  }
+  this.setState({
+    showNumber:[result],
+    process :"",
+    oneElemnt:[result],
+    towElemnt:[] 
+  })
+  }
+
+
 
   render() {
     return (
       <div className="calculator">
-        <Display />
-        <NumberPad />
+        <Display theShow={this.state.showNumber}/>
+        <NumberPad TheRenderProcsess={this.renderProcsess.bind(this)} />
+        
       </div>
     );
   }
 }
+
 
 export default Calculator;
