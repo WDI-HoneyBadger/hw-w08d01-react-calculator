88d3efb903c79eb7959ed3f90e4c40c202a0d6ec
rawandahli <rawandahlii@gmail.com>
2019-01-08 01:33:13 +0300

    hw

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,18 +3,112 @@ import Display from './Display';
 import NumberPad from './NumberPad';
 
 class Calculator extends Component {
-  // this component may need to have some state
+
   // think about what you need to keep track of
   // where would you need to pass information to?
 
-  render() {
-    return (
-      <div className="calculator">
-        <Display />
-        <NumberPad />
-      </div>
-    );
+
+  // this component may need to have some state
+  constructor() {
+    super();
+    this.state = {
+      displayNumber =[],
+      operation = '',
+      first =[],
+      second =[]
+    }
+  }
+
+
+
+
+  renderCalc(symbol, number) {
+    // console.log(this.state)
+    if ((number === true || symbol === ".") && this.state.operation === '') {
+      {
+        const newNumber = this.state.first.concat.([symbol])
+        this.setState({
+          displayNumber: newNumber,
+          first: newNumber
+        })
+      } else if (symbol === "=") {
+        // render result ha4i function ga3deen nnadeha
+        this.renderResult();
+      } else if (symbol === "C") {
+        this.setState({
+          displayNumber =[],
+          operation = '',
+          first =[],
+          second =[]
+        })
+      } else if (symbol !== 'C' && number === false && symbol !== '.') {
+        this.setState({
+          operation: symbol
+        })
+
+      } else if ((number === true || '.') && this.state.operation !== '') {
+        const newNumber = this.state.second.concat([symbol])
+        this.setState({
+          displayNumber: newNumber,
+          second: newNumber
+        })
+      }
+    }
+    renderResult(){
+      let first = this.state.first;
+      let second = this.state.second;
+      let operation = this.state.operation;
+
+      first = first.join('');
+      second = second.join('');
+      //parseFloat can be used instead
+
+
+      first = Number(first);
+      second = Number(second);
+
+
+      let result;
+
+      if (operatin === '+') result = first + second
+      else if (operation === '-') result = first - second
+      else if (operation === '+') result = first + second
+      else if (operation === '/') result = first / second
+      else if (operation === '*') result = first * second
+
+
+      this.setState({
+        displayNumber =[result],
+        operation = '',
+        first =[result],
+        second =[]
+      })
+      //  console.log(first + opreation + second);
+      //  console.log(result);
+    }
+
+    render() {
+      return (
+        <div className="calculator">
+          <Display displaySymbol={this.state.displayNumber} />
+          <NumberPad renderCalc={this.renderCalc.bind(this)} />
+          <Display />
+          <NumberPad />
+
+        </div>
+      );
+    }
   }
 }
+  
+
+return (
+  <div className="calculator">
+    <Display />
+    <NumberPad />
+  </div>
+);
+
+
 
 export default Calculator;
--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,9 @@ const Display = (props) => {
   return (
     <div className="display">
       <h5 className="display_text">
-        3.14
+     {/* to see the number  */}
+      {props.displaySymbol}
+        {/* 3.14  << we wont need this */}
       </h5>
     </div>
   );
