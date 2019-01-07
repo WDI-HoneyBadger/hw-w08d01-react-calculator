fc3abaa3fb05e58eb3e81a433bac4592e6ed1fe4
MayMo7a <maymo7a@gmail.com>
2019-01-07 23:41:09 +0300

    HW Done!

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,11 +7,79 @@ class Calculator extends Component {
   // think about what you need to keep track of
   // where would you need to pass information to?
 
+  constructor(){
+    super()
+    this.state = {
+      displayNum: 0,
+      inpNumOne : '',
+      inpNumTwo: '',
+      operator: ''
+    }
+  }
+  renderInps(symbol, num){
+    if((num === true || symbol === '.') && this.state.operator ===''){
+      const newNum = this.state.inpNumOne.concat(symbol);
+      this.setState({
+        displayNum: newNum,
+        inpNumOne: newNum
+      })
+    } else if (symbol === 'C'){
+      this.setState({
+        displayNum: 0,
+        inpNumOne : '',
+        inpNumTwo: '',
+        operator: ''
+      })
+    } else if (symbol === '='){
+      this.claculateInps();
+    } else if (num === false && symbol !== '.'){
+      this.setState({
+        operator: symbol
+      })
+    } else if (this.state.operator !== ''){
+      const newNum = this.state.inpNumTwo.concat(symbol);
+      this.setState({
+        displayNum: newNum,
+        inpNumTwo: newNum
+      })
+    }
+  }
+
+  claculateInps(){
+    const inpNumOne = this.state.inpNumOne;
+    const inpNumTwo = this.state.inpNumTwo;
+    const operator = this.state.operator;
+
+    
+    let result = 0;
+
+    if (operator === '+'){
+      result = inpNumOne + inpNumTwo
+    } else if (operator === '-'){
+      result = inpNumOne - inpNumTwo
+    } else if (operator === '/') {
+      result = inpNumOne / inpNumTwo
+    } else if (operator === 'x'){
+      result = inpNumOne * inpNumTwo
+    } else if (operator === '%'){
+      result = inpNumOne % inpNumTwo
+    }
+
+    this.setState({
+      displayNum: result,
+      inpNumOne: result,
+      inpNumTwo: '',
+      operator: ''
+    })
+
+
+  }
+
   render() {
     return (
       <div className="calculator">
-        <Display />
-        <NumberPad />
+        <Display displayNum={this.state.displayNum} />
+        <NumberPad  renderInps={this.renderInps.bind(this)}/>
       </div>
     );
   }
--- src/components/NumberPad.js
+++ src/components/NumberPad.js
@@ -7,7 +7,10 @@ class NumberPad extends Component {
   renderTiles() {
     return tileData.map((tile, i) => {
       return (
-        <Tile key={i} size={tile.size} symbol={tile.symbol} />
+        <div key={i} 
+        onClick={() => {this.props.renderInps(tile.symbol, tile.number)}}>
+          <Tile key={i} size={tile.size} symbol={tile.symbol} />
+        </div>
       )
     });
   }
